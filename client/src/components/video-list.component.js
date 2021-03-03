e1b368d025aeeab587948f4350bd6703ec56bbae
Eduardo Haddad <eduardo.torquemada@gmail.com>
2021-03-03 11:42:16 -0300

    Alternar vídeo por idioma se houver múltiplos

--- client/src/components/video-list.component.js
+++ client/src/components/video-list.component.js
@@ -154,6 +154,16 @@ export default function VideoList(props) {
         setVideoList(props.lista.data.videos);
     };
 
+    // #5 if lang changes, switch video id with language specific id (if present)
+    useEffect(() => {
+        videoList.map(video => {
+            let vLang = video[lang][`vimeo_id_${lang}`];
+            if(vLang !== '' && typeof vLang !== "undefined"){
+                video.id = vLang;
+            }
+        });
+    }, [lang]);
+
     const PreviewContainer = styled.div`
         ${BP.small} {
             & > div {
